ec91bf8303a3a513f9b78af380843b42194532c0
Amelia Baldwin <amelia01000010@gmail.com>
2022-09-08 16:10:10 +0100

    addNewEmployee function added

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,21 +1,52 @@
-import React from 'react';
-import './Card.scss';
-import Counter from '../Counter/Counter';
+import React from "react";
+import "./Card.scss";
+import { useState } from "react";
+import Counter from "../Counter/Counter";
 
 const Card = (props) => {
-  const {myTeam} = props;
-  const cardListJSX = myTeam.map((team, index) => (
-    <div className='cards-content'>
-      <h3 key={index} className='card_content'>Name: {team.name}</h3>
-      <h3 key={index} className='card_content'>Role: {team.role}</h3>
-      <Counter/>
+  const { teamArr } = props;
+  const [newArr, setNewArr] = useState(teamArr);
+
+  const addNewEmployee = () => {
+    const newEmployeeName = document.getElementById("newEmployeeName");
+    const newEmployeeRole = document.getElementById("newEmployeeRole");
+    const employee = {
+      id: 11,
+      name: newEmployeeName.value,
+      role: newEmployeeRole.value,
+    };
+    newEmployeeName.value = "";
+    newEmployeeRole.value = "";
+    setNewArr([...newArr, employee]);
+  };
+
+  const cardListJSX = newArr.map((team, index) => (
+    <div key={index} className="cards_content">
+      <h3 className="card_content">Name: {team.name}</h3>
+      <h3 className="card_content">Role: {team.role}</h3>
+      <Counter />
     </div>
   ));
+
   return (
-    <div className='cards'>
-      {cardListJSX}
+    <div>
+      <input id="searchEmployees" type="text" />
+      <div>
+        <div className="newEmployee">
+          <h2>New Employee Name</h2>
+          <input id="newEmployeeName" type="text" />
+        </div>
+        <div className="newEmployee">
+          <h2>New Employee Role</h2>
+          <input id="newEmployeeRole" type="text" />
+        </div>
+        <div onClick={addNewEmployee}>
+          <button>Add new Employee</button>
+        </div>
+      </div>
+      <div className="cards">{cardListJSX}</div>
     </div>
-  )
-}
+  );
+};
 
 export default Card;
